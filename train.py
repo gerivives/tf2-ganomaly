a2e0c033d575ca0fc450865b22c7de620c106861
Gerard Vives <gerardvivesp@gmail.com>
2024-01-06 11:54:25 +0100

    Adapted train.py for a new VOIP dataset

--- train.py
+++ train.py
@@ -48,7 +48,6 @@ def main(_):
         logging.get_absl_handler().use_absl_log_file(FLAGS.dataset, log_dir=FLAGS.log_dir)
 
     files = sorted([k for k in os.listdir(DATASET_PATH) if k.endswith(DATASET_EXTENSION)])
-    print(files)
     sets = [pd.read_csv(DATASET_PATH + file) for file in files]
     frames = pd.concat(sets, axis=0, ignore_index=True)
     columns = frames.columns.to_list()
@@ -65,7 +64,7 @@ def main(_):
 
     columns = frames.columns.to_list()
     FLAGS.isize = len(columns)-1
-
+    print("The label is: " + label)
     labels = sorted(frames[label].unique().tolist())
     print(frames[label].value_counts())
     print('Final labels used: ' + ', '.join(labels))
